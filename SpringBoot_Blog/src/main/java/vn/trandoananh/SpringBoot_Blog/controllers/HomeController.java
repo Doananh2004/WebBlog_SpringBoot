7fa33339fdc19b7521fa0bc5bc8c9b3dfcbe5dc5
Doananh2004 <trandoananhnt@gmail.com>
2025-05-23 21:39:15 +0700

    Thêm chức năng thống kê

--- SpringBoot_Blog/src/main/java/vn/trandoananh/SpringBoot_Blog/controllers/HomeController.java
+++ SpringBoot_Blog/src/main/java/vn/trandoananh/SpringBoot_Blog/controllers/HomeController.java
@@ -1,12 +1,25 @@
 package vn.trandoananh.SpringBoot_Blog.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import vn.trandoananh.SpringBoot_Blog.repository.CategoryRepository;
+import vn.trandoananh.SpringBoot_Blog.repository.PostRepository;
 
 @Controller
 public class HomeController {
-	@GetMapping("/")
-    public String home() {
-        return "index"; // trỏ tới index.html
+	@Autowired
+    private PostRepository postRepository;
+
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    @GetMapping("/")
+    public String home(Model model) {
+        model.addAttribute("postCount", postRepository.count());
+        model.addAttribute("categoryCount", categoryRepository.count());
+        return "index";
     }
 }
